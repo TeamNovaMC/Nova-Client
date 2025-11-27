b14413b0f484e28d124c6f37701356b83fd73aab
RadiantByte <radiantbyte@outlook.com>
2025-11-27 09:28:13 -0700

    update protocol

--- relay/Protocol/bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/packet/BedrockPacketHandler.java
+++ relay/Protocol/bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/packet/BedrockPacketHandler.java
@@ -908,7 +908,7 @@ public interface BedrockPacketHandler extends PacketHandler {
         return PacketSignal.UNHANDLED;
     }
 
-    default PacketSignal handle(ServerScriptDebugDrawerPacket packet) {
+    default PacketSignal handle(DebugDrawerPacket packet) {
         return PacketSignal.UNHANDLED;
     }
 
@@ -921,6 +921,10 @@ public interface BedrockPacketHandler extends PacketHandler {
     }
 
     default PacketSignal handle(ServerPlayerPostMovePositionPacket packet) {
+        return PacketSignal.UNHANDLED;
+    }
+
+    default PacketSignal handle(ServerScriptDebugDrawerPacket packet) {
         return PacketSignal.UNHANDLED;
     }
 }
